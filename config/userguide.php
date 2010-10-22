a23b25ef25a83887f5d89860e1294f3cc749b8f4
bluehawk <phroughy@gmail.com>
2010-10-21 20:12:03 -0600

    Updated userguide description

--- config/userguide.php
+++ config/userguide.php
@@ -14,7 +14,7 @@ return array(
 			'name' => 'Auth',
 
 			// A short description of this module, shown on the index page
-			'description' => 'Official Auth module, used for user authentication and authorization.',
+			'description' => 'User authentication and authorization.',
 			
 			// Copyright message, shown in the footer for this module
 			'copyright' => '&copy; 2008–2010 Kohana Team',
