5ee08a93db03cb98a5c25c0a75dd5c9bd4de1847
Woody Gilk <woody.gilk@kohanaframework.org>
2012-03-18 22:00:09 -0500

    Partial revert of a981cf2b4906f821b72c2ca1b0f3a2f5268b034e. Put "lifetime" back into auth config, apparantly ORM uses it for something completely unrelated

--- config/auth.php
+++ config/auth.php
@@ -5,6 +5,7 @@ return array(
 	'driver'       => 'file',
 	'hash_method'  => 'sha256',
 	'hash_key'     => NULL,
+	'lifetime'     => 1209600,
 	'session_type' => Session::$default,
 	'session_key'  => 'auth_user',
 
